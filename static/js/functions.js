ce726bf6cb0dd85c6b1d3420072f853548215fbe
roodhouse <rughjm@gmail.com>
2023-11-24 12:32:15 -0600

    header work

---
+++ static/js/functions.js
@@ -0,0 +1,11 @@
+function toggleClass(event) {
+    console.log('hi')
+    console.log(event)
+    if(event.target.id == 'ham') {
+        if(event.target.classList.contains('active')) {
+            console.log('ham')
+            // remove the active class and place inactive class on
+            // get the x element, remove inactive and place active on
+        }
+    }
+}
