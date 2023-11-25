8ea36cae115aa815f96f8a1305c81f4878b64af5
roodhouse <rughjm@gmail.com>
2023-11-24 18:22:43 -0600

    more header work

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,15 +1,19 @@
 function toggleClass(event, currentId, otherId) {
-    console.log(currentId)
+
     let currentElement = document.getElementById(currentId)
     let otherElement = document.getElementById(otherId)
     let mobileMenu = document.getElementById('mobileMenu')
+    let logo = document.getElementById('navImg')
+    
     if(!currentElement.classList.contains('inactive')) {
             currentElement.classList.replace('active', 'inactive')
             otherElement.classList.replace('inactive', 'active')
-    }
+    } 
     if (currentId === 'ham') {
         mobileMenu.classList.replace('inactive', 'active')
+        logo.style.visibility = 'hidden'
     } else {
         mobileMenu.classList.replace('active', 'inactive')
+        logo.style.visibility = 'visible'
     }
 }
