fbde87558d8cfefd90decc6850d85ba87a4ea94f
roodhouse <rughjm@gmail.com>
2023-11-30 13:06:56 -0600

    email working

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, request
+from os import getenv
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, TextAreaField
 from wtforms.validators import DataRequired
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -340,6 +347,25 @@ with app.app_context():
 
 #     db.session.commit()
 
+def send_email(name, message, email, to_email):
+    print(email)
+    email_acct = getenv('EMAIL')
+    from_email = email
+    password = getenv('PASSWORD')
+    smtp_server = getenv('SERVER')
+    port = getenv('PORT')
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = to_email
+    msg['Name'] = name
+    msg['Subject'] = 'New comment from portfolio'
+    msg.attach(MIMEText(message, 'plain'))
+
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls()
+        server.login(email_acct, password)
+        server.sendmail(from_email, to_email, msg.as_string())
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = NewContactForm()
@@ -365,14 +391,32 @@ def home():
 
             print(new_contact)
 
-            with app.app_context():
-                try:
-                    db.session.add(new_contact)
-                    db.session.commit()
-                    return redirect('/')
-                except Exception as e:
-                    print('failure:', e)
-                    db.session.rollback()
+            try:
+                send_email(name, message, email, 'rughjm@gmail.com')
+                print(jsonify({'message': 'Email sent'}))
+                
+                with app.app_context():
+                    try:
+                        db.session.add(new_contact)
+                        db.session.commit()
+                        return redirect('/')
+                    except Exception as e:
+                        print('failure:', e)
+                        db.session.rollback()
+
+            except Exception as e:
+                print(jsonify({'error': 'Failed to send email', 'details': str(e)}), 500)
+                return redirect('/')
+
+            # with app.app_context():
+            #     try:
+            #         db.session.add(new_contact)
+            #         db.session.commit()
+            #         return redirect('/')
+            #     except Exception as e:
+            #         print('failure:', e)
+            #         db.session.rollback()
+
         else:
             return render_template("index.html", skills=all_skills, work=all_work, project=all_project, form=form)
     return render_template('index.html', form=form)
