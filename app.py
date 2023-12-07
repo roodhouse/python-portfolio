e0106ccd8bc9cd5974d0090cf47d158ddc2d510e
roodhouse <rughjm@gmail.com>
2023-12-07 11:48:02 -0600

    requirements

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-
-# generate requirements.txt
 # prepare server on AWS
 # move publish and move site
 
